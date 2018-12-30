48b247442084caf1de8c3b02b1038cc826633165
zmf <zoheiramz.zm@gmail.com>
2018-12-30 08:30:05 +0330

    Add two functions

--- fuctions.py
+++ fuctions.py
@@ -1,5 +1,8 @@
 from Node import Node
 
+te = ''
+le = []
+
 
 def tree(node2, node1):
     node = Node(node1.index + node2.index)
@@ -20,3 +23,34 @@ def write():
     None
 
 
+def write_binary(inp, file):
+    ls = []
+    while inp != '':
+        if (len(inp) // 4 == 0) and (len(inp) % 4 != 0):
+            result = ''
+            for i in range(4 - (len(inp) % 4)):
+                result += '0'
+            result += inp
+            inp = ''
+        else:
+            inp, result = inp[:-4], inp[-4:]
+        temp = int(result[::-1], 2).to_bytes(4, 'big')
+        ls.append(temp)
+    ls.reverse()
+    for byte in ls:
+        file.write(byte)
+
+
+def terav(no):
+    global te
+    global le
+    if no is None:
+        te += '0'
+        return te
+    else:
+        te += '1'
+        if no.value is not None:
+            le.append(no.value)
+        terav(no.left)
+        terav(no.right)
+        return te, le
