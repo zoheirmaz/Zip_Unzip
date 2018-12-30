244022b8418499d05d0ef5f23ed04167ab142c59
zmf <zoheiramz.zm@gmail.com>
2018-12-30 08:30:36 +0330

    etc

--- test.py
+++ test.py
@@ -1,24 +1,71 @@
-# f = open('spam.txt', encoding='latin1')
-f = open('a.png', mode='rb')
-# a = f.read()
-# print(a)
-# print(type(a))
-#
-# a = bytes(a)
-#
-# print(a)
-# print(type(a))
+from binarytree import tree, Node
+from bitstring import BitArray
 
-x = []
 
-# a=f.read()
-# print(a)
-# print(bytearray(a))
+te = ''
+lst = None
 
-print(f.__sizeof__())
-a=''
-while a != b'':
-    a=f.read(1)
-    x.append(bytes(a))
 
-print('oK')
+
+
+def reverse(string):
+    string = string[::-1]
+    return string
+
+
+def terav(no):
+    global te
+    if no is None:
+        te += '-1 '
+        return te
+    te += str(no.value) + ' '
+    terav(no.left)
+    terav(no.right)
+    return te
+
+
+def getList(ls):
+    text = ''
+    cursor = ''
+    if ls.__len__() != 0:
+        cursor = ls.pop()
+        # text += cursor
+
+    while (cursor != ' ') and (ls.__len__() != 0):
+        text += cursor
+        cursor = ls.pop()
+
+    if text == '':
+        # print('None')
+        return None
+    else:
+        # print(text)
+        return int(text)
+
+
+def cre():
+    x = getList(lst)
+    if x == -1:
+        return None
+    node = Node(x)
+    node.left = cre()
+    node.right = cre()
+    return node
+
+def get4(inp):
+    inp, result = inp[:-4], inp[-4:]
+    return inp,result
+
+if __name__ == '__main__':
+    # node1 = tree(4)
+    #
+    # print(node1)
+    # lst = terav(node1)
+    # print(lst)
+    # s = ''
+    # lst = list(lst)
+    # lst.reverse()
+    #
+    # print(cre())
+    print(get4('0123456789'))
+
